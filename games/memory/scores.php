dc6f89219e189a44b9709f8d243ac632866b523a
Elwan Beauchot <elwan.beauchot-le-bon@edu.esiee-it.fr>
2023-10-25 09:46:51 +0200

    Page d'inscription fonctionnelle

--- games/memory/scores.php
+++ games/memory/scores.php
@@ -7,10 +7,15 @@ require_once SITE_ROOT . 'partials/header.php';
 require_once SITE_ROOT . 'chat.php';
 require_once SITE_ROOT . 'utils/database.php';
 
-$pdoTablo = $pdo->prepare('SELECT Jeu.nomJeu, pseudo, difficulte, score , dateHeure
+if (!isset($_GET['recherche_pseudo'])){
+    $_GET['recherche_pseudo'] = '';
+}
+
+$pdoTablo = $pdo->prepare('SELECT Jeu.nomJeu, pseudo, difficulte, score , dateHeureScore
     FROM Scores
     INNER JOIN Jeu ON Scores.idJeu=Jeu.id
     INNER JOIN Utilisateur ON Scores.idJoueur=Utilisateur.id
+    WHERE pseudo LIKE "%":pseudo"%"
     ORDER BY Jeu.nomJeu ASC,
     CASE
         WHEN difficulte="easy" THEN 1
@@ -18,33 +23,24 @@ $pdoTablo = $pdo->prepare('SELECT Jeu.nomJeu, pseudo, difficulte, score , dateHe
         WHEN difficulte="hard" THEN 3 END,
     score ASC;');
 
-
-$pdoTablo->execute([]);
+$pdoTablo->execute([':pseudo' => $_GET['recherche_pseudo']]);
 $scoreTablo = $pdoTablo->fetchAll();
-/*
-    foreach($scoreTablo as $score){
-        echo $score -> nomJeu;
-        echo $score -> pseudo;
-        echo $score -> difficulte;
-        echo $score -> score;
-
-    }
-    */
-
-
-
-
 
 ?>
+
 <section class="scoreshtml">
 
     <body>
-
-
         <div class="scores_titre">
             SCORES
         </div>
         <div class="scores">
+            <form method="get" style="width: 100%;">
+                <span class="scores_recherche">
+                    <img src="<?= PROJECT_FOLDER ?>assets/images/recherche.png">
+                    <input type="text" name="recherche_pseudo" placeholder="Recherche" class="scores_recherche_entree">
+                </span>
+            </form>
             <table class="styled-table">
 
                 <thead>
@@ -62,7 +58,7 @@ $scoreTablo = $pdoTablo->fetchAll();
                         <td><?php echo $scoreTablo[$i]->pseudo ?> </td>
                         <td><?php echo $scoreTablo[$i]->difficulte ?> </td>
                         <td><?php echo $scoreTablo[$i]->score ?> </td>
-                        <td><?php echo $scoreTablo[$i]->dateHeure ?> </td>
+                        <td><?php echo $scoreTablo[$i]->dateHeureScore ?> </td>
                     </tr>
                 <?php
                 }
--- register.php
+++ register.php
@@ -5,6 +5,17 @@ require_once 'utils/common.php';
 require_once SITE_ROOT . 'partials/head.php';
 require_once SITE_ROOT . 'partials/header.php';
 require_once SITE_ROOT . 'chat.php';
+require_once SITE_ROOT . 'utils/database.php';
+
+
+
+
+
+$pdoPseudoExistant = $pdo->prepare('SELECT pseudo FROM Utilisateur WHERE pseudo = :pseudo;');
+$pdoEmailExistant = $pdo->prepare('SELECT email FROM Utilisateur WHERE email = :email;');
+$pdoInsertionUtilisateur = $pdo->prepare('INSERT INTO Utilisateur (email, mdp, pseudo) VALUES(:email, :mdp, :pseudo)');
+
+
 ?>
 <section class="registerhtml">
 
@@ -13,16 +24,77 @@ require_once SITE_ROOT . 'chat.php';
             INSCRIPTION
         </div>
         <div class="inscription">
-            <form action="myAccount.php" style="width: 100%;">
-                <input type="email" placeholder="Email" required="required" class="inscription_entree">
-                <input type="text" placeholder="Pseudo" required="required" class="inscription_entree">
-                <input type="password" placeholder="Mot de passe" required="required" class="inscription_entree">
-                <input type="password" placeholder="Confirmez le mot de passe" required="required" class="inscription_entree">
-                <input type="submit" value="Inscription" class="inscription_bouton">
-            </form>
-            <span style="font-size: 2vmin;">
-                Vous avez déjà un compte ? <a href="login.php" class="register_link">Connectez-vous</a> !
-            </span>
+            <?php if ($_POST == NULL) : ?>
+
+                <form method="post" style="width: 100%;">
+                    <input type="email" name="email" placeholder="Email" required="required" class="inscription_entree">
+                    <input type="text" name="pseudo" placeholder="Pseudo" required="required" class="inscription_entree">
+                    <input type="password" name="mdp" placeholder="Mot de passe" required="required" class="inscription_entree">
+                    <input type="password" name="mdp_confirme" placeholder="Confirmez le mot de passe" required="required" class="inscription_entree">
+                    <input type="submit" value="Inscription" class="inscription_bouton">
+                </form>
+                <span style="font-size: 2vmin;">
+                    Vous avez déjà un compte ? <a href="login.php" class="register_link">Connectez-vous</a> !
+                </span>
+                <?php else :
+                $pdoPseudoExistant->execute([':pseudo' => $_POST['pseudo']]);
+                $pseudoExistant = $pdoPseudoExistant->fetch();
+
+                $pdoEmailExistant->execute([':email' => $_POST['email']]);
+                $emailExistant = $pdoEmailExistant->fetch();
+
+                        
+
+                if (
+                    preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/', $_POST['mdp'])
+                    &&
+                    preg_match('/^.{4,}$/', $_POST['pseudo'])
+                    &&
+                    ($_POST['mdp'] == $_POST['mdp_confirme'])
+                    &&
+                    ($pseudoExistant == NULL)
+                    &&
+                    ($emailExistant == NULL)
+                ) :
+                    $_POST['mdp'] = hash('sha256', $_POST['mdp']);
+                    $pdoInsertionUtilisateur ->execute([':email'=> $_POST['email'], ':mdp'=> $_POST['mdp'], ':pseudo'=> $_POST['pseudo']]);
+                ?>
+                    <p style="font-size: 2vmin;">Vous êtes bien inscrit!</p>
+                    
+
+                <?php else : ?>
+
+                    <form method="post" style="width: 100%;">
+                        <input type="email" value="<?= $_POST['email'] ?>" name="email" placeholder="Email" required="required" class="inscription_entree">
+                        <?php if ($emailExistant != NULL) : ?>
+                            <p style="font-size: 2vmin; color:red">Email déjà utilisé</p>
+                        <?php endif; ?>
+                        <input type="text" value="<?= $_POST['pseudo'] ?>" name="pseudo" placeholder="Pseudo" required="required" class="inscription_entree">
+
+                        <?php if (!preg_match('/^.{4,}$/', $_POST['pseudo']) || ($pseudoExistant != NULL)) : ?>
+                            <p style="font-size: 2vmin; color:red">Pseudo trop court ou déjà utilisé</p>
+                        <?php endif; ?>
+
+                        <input type="password" name="mdp" placeholder="Mot de passe" required="required" class="inscription_entree">
+
+                        <?php if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$/', $_POST['mdp'])) : ?>
+                            <p style="font-size: 2vmin; color:red">Format de mot de passe invalide</p>
+                        <?php endif; ?>
+
+                        <input type="password" name="mdp_confirme" placeholder="Confirmez le mot de passe" required="required" class="inscription_entree">
+
+                        <?php if ($_POST['mdp'] != $_POST['mdp_confirme']) : ?>
+                            <p style="font-size: 2vmin; color:red">Les mots de passe ne correspondent pas</p>
+                        <?php endif; ?>
+
+                        <input type="submit" value="Inscription" class="inscription_bouton">
+                    </form>
+                    <span style="font-size: 2vmin;">
+                        Vous avez déjà un compte ? <a href="login.php" class="register_link">Connectez-vous</a> !
+                    </span>
+                <?php endif; ?>
+
+            <?php endif; ?>
         </div>
         <?php require_once SITE_ROOT . 'partials/footer.php'; ?>
     </body>
